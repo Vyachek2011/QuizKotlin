3d733671d4e54a818d4ee5a1ed4b4fd7f8b05a4e
Gudkov <Slava_Gudkov@mail.ru>
2020-07-30 17:09:09 +0300

    Добавлен Toast

--- app/src/main/java/com/example/quizkotlin/MainActivity.kt
+++ app/src/main/java/com/example/quizkotlin/MainActivity.kt
@@ -6,9 +6,13 @@ import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.OnBackPressedDispatcher
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var TimeBackPressed: Long = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +26,20 @@ class MainActivity : AppCompatActivity() {
         //MyIntent.putExtra(GameLevels.TOTAL_THIRD, 123)
         startActivity(IntentGameLevels);
         finish();
+    }
+
+    override fun onBackPressed(): Unit  {
+        var BackToast = Toast.makeText(baseContext, "Надмите еще раз, чтобы выйти", Toast.LENGTH_SHORT);
+
+        if ((TimeBackPressed + 5000) > System.currentTimeMillis()) {
+            BackToast.cancel();
+            super.onBackPressed();
+        }
+        else {
+            BackToast.show();
+        }
+
+        TimeBackPressed = System.currentTimeMillis();
     }
 
 }
